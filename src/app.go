92d90df897163845cfce08f5e60d8aa8412f50bc
iganbold <itgel.ganbold1@gmail.com>
2016-04-13 05:09:57 +0000

    Add websocket and push tweets to front end

--- src/app.go
+++ src/app.go
@@ -2,39 +2,81 @@ package main
 
 import (
     "fmt"
-    "net/http"  
-    "time"
+    "net/http"
+    "reflect"
 
     "github.com/op/go-logging"
     "github.com/gorilla/websocket"
+    "github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
+// Websocket Upgrade buffer
 var upgrader = websocket.Upgrader {}
+
 var log = logging.MustGetLogger("Twitter Tweeds Log")
 
-
+// Listen and Serve 
 func main() {
     http.HandleFunc("/", home)
     http.HandleFunc("/twitter/stream", twitterStream)
-    
+
     http.ListenAndServe(":8080",nil)
 }
 
+// Serve home page
 func home(res http.ResponseWriter, req *http.Request) {
+    res.Header().Set("Access-Control-Allow-Origin", "*")
     http.ServeFile(res,req, "../public/index.html")
 }
 
+// Serve Websocket Twitter stream
 func twitterStream(res http.ResponseWriter, req *http.Request) {
     log.Info("WS Twitter Request: ",req.URL.Query())
     filter := req.URL.Query()["filter"]
-    
     if filter != nil {
-        
         // Upgrades the http server connection to the websocket protocol 
         conn, _ := upgrader.Upgrade(res, req, nil)
         
-        go wsReader(conn,filter[0])
-        go wsWriter(conn,filter[0])
+        consumerKey := ""
+    	consumerSecret := ""
+    	accessToken := ""
+    	accessSecret := ""
+    	
+        if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+    		log.Error("Consumer key/secret and Access token/secret required")
+    	}
+    
+    	config := oauth1.NewConfig(consumerKey, consumerSecret)
+    	token := oauth1.NewToken(accessToken, accessSecret)
+    	
+    	httpClient := config.Client(oauth1.NoContext, token)
+    	
+    	client := twitter.NewClient(httpClient)
+    	
+    	demux := twitter.NewSwitchDemux()
+    	demux.Tweet = func(tweet *twitter.Tweet) {
+    		fmt.Println("Stream/"+filter[0]+":",tweet.Text)
+    		wsWriter(conn,tweet.Text)
+    	}
+    	
+    	fmt.Println("Starting Stream...")
+    	
+    	// Filter
+    	filterParams := &twitter.StreamFilterParams{
+    		Track:         []string{filter[0]},
+    		StallWarnings: twitter.Bool(true),
+    	}
+    	
+    	stream, err := client.Streams.Filter(filterParams)
+    	if err != nil {
+    		log.Error(err)
+    	}
+    	
+    	fmt.Println("Type:", reflect.TypeOf(stream))
+        
+        go wsReader(conn,filter[0],stream)      // Read messages from the websocket or stop websoket and twitter stream
+    	go demux.HandleChan(stream.Messages)    // Receive twitter messages until stream quits
         
     } else {
         fmt.Fprintf(res, "Warning: Filter param is required to stream the data from Twitter")
@@ -42,36 +84,30 @@ func twitterStream(res http.ResponseWriter, req *http.Request) {
 }
 
 // Read messages from the websocket
-func wsReader(conn *websocket.Conn,filter string) {
+func wsReader(conn *websocket.Conn,filter string,stream *twitter.Stream) {
     for {
         _, _, err := conn.ReadMessage()
         
         //Close the websocket connection
         if err != nil {
             conn.Close()
-            log.Info("WS Connection Closed: "+filter)
-            // fmt.Println("WS Connection Closed:",filter)
+            stream.Stop()
+            log.Info("wsReader/WS Connection Closed: "+filter)
+            log.Info("wsReader/Twitter Stream Closed: "+filter)
             break
         } else {
             log.Error(err)
         }
     }    
 }
+
 // Write messages to the websocket
 func wsWriter(conn *websocket.Conn,filter string) {
-    for {
-        ch := time.Tick(5 *time.Second)
-        
-        for range ch {
-            conn.WriteJSON(myStruct{
-                Name: filter,
-            })
-        }
-    }    
+    conn.WriteJSON(tweetStruct{
+        Tweet: filter,
+    })      
 }
 
-
-
-type myStruct struct {
-    Name string
+type tweetStruct struct {
+    Tweet string
 }
