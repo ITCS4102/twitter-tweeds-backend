c50b2ea8d7eb05c1dac43e26b8de2c2307620edf
iganbold <itgel.ganbold1@gmail.com>
2016-04-17 04:22:40 +0000

    Update main.go port

--- main.go
+++ main.go
@@ -49,16 +49,16 @@ var globalTwitterStream *twitter.Stream
 
 // Start the app then Listen and Serve 
 func main() {
-//     port := os.Getenv("PORT")
+    port := os.Getenv("PORT")
     
-//     if port == "" {
-// 		log.Fatal("$PORT must be set")
-// 	}
+    if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	
     http.HandleFunc("/", home)
     http.HandleFunc("/twitter/stream", twitterStream)
     fmt.Println("TweetDeck started and ready to rock");
-    http.ListenAndServe(":8080",nil)
+    http.ListenAndServe(port,nil)
 }
 
 // Serve home page
